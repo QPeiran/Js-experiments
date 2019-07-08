0dbad009622a162d41794a1ff9c572f02844265b
Peiran Quan <quanpeiran@gmail.com>
2019-07-08 16:54:17 +1200

    Update and rename fetch-test.js to fetch VS XML Request.js

---
+++ fetch VS XML Request.js
@@ -0,0 +1,44 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+
+
+//++++++++++++++++++++++++++++++++++++++++
+
+// Set up our HTTP request
+var xhr = new XMLHttpRequest();
+
+// Setup our listener to process compeleted requests
+xhr.onreadystatechange = function () {
+
+	// Only run if the request is complete
+	if (xhr.readyState !== 4) return;
+
+	// Process our return data
+	if (xhr.status >= 200 && xhr.status < 300) {
+		// What do when the request is successful
+		console.log(JSON.parse(xhr.responseText));
+	}
+
+};
+
+// Create and send a GET request
+// The first argument is the post type (GET, POST, PUT, DELETE, etc.)
+// The second argument is the endpoint URL
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+xhr.send();
